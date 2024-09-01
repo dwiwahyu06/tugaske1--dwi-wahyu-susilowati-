d0862d758bbda72611a43d8aa953163c92ae9395
PUB 0820 <PUB0820@hotmail.com>
2024-09-01 09:37:00 +0700

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Profil from "./Profil";
+
+export default function App() {
+  return (
+    <section>
+      <h1 className="h1">Pahlawan indonesia</h1>
+      <Profil/>
+    </section>
+  );
+}
---
+++ src/Profil.jsx
@@ -0,0 +1,18 @@
+export default function Profil() {
+    return (
+        <div className = "containerr">
+            <div>
+                <img src="https://o-cdn-cas.sirclocdn.com/parenting/images/pahlawan_soekarno.width-500.format-webp.webp" alt=""/>
+                <h2>Ir.Soekarno</h2>
+            </div>
+            <div>
+                <img src="https://akcdn.detik.net.id/community/media/visual/2022/08/06/laksamana-laut-rd-eddy-martadinata.jpeg?w=464" alt=""/>
+                <h2>laksamana laut R.D. eddy Martadinata</h2>
+            </div>
+            <div>
+                <img src="https://kompaspedia.kompas.id/wp-content/uploads/2021/04/89005p.jpg" alt=""/>
+                <h2>Raden Ajeng Kartini</h2>
+            </div>
+      </div>
+    );
+  }
